ee4cba0576ba63b998dbd7278a76a948090e9630
mananjabrik <manan.jabrik@gmail.com>
2024-06-18 02:23:38 +0700

    update

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -1,10 +1,29 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button } from './theme';
 import { isAddress } from 'viem';
+import axios, { AxiosError } from 'axios';
 export const Faucet = () => {
-  const [address, setAddress] = useState<string>();
-  const triggerClaim = async () => {
-    console.log(isAddress(address ?? ''));
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const triggerClaim = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(e.currentTarget);
+    const address = formData.get('address');
+    try {
+      const claim = await axios.post<
+        any,
+        {
+          status: number;
+          data: boolean;
+        }
+      >(`http://localhost:3000/api/transaction/${address}`);
+    } catch (e: any | AxiosError) {
+      if (axios.isAxiosError(e)) {
+        return alert(e?.response?.data?.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -12,20 +31,23 @@ export const Faucet = () => {
       <div className="pb-8">
         <h1 className="text-xl text-center">Faucet</h1>
       </div>
-      <form>
+      <form onSubmit={triggerClaim}>
         <div className="flex flex-col">
           <div className="flex-1">
-            <label className="block mb-2 text-sm font-medium">Address</label>
+            <label className="block mb-2 text-sm font-medium" htmlFor="address">
+              Address
+            </label>
           </div>
           <input
             className="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="text"
-            onChange={(e) => setAddress(e.currentTarget.value)}
             placeholder="0x...000"
+            name="address"
+            required
           />
         </div>
-        <Button theme="primary" option="mt-2 w-full" onClick={triggerClaim}>
-          Claim
+        <Button theme="primary" option="mt-2 w-full">
+          {isLoading ? 'loading . . .' : 'Claim ( 0.1 )'}
         </Button>
       </form>
     </div>
